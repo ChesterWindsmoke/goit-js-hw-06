5d5f3daf272c77923dfa247333f21b1327c1c33d
ChesterWindsmoke <vladislavmarchenko999@gmail.com>
2023-05-07 02:19:54 +0300

    commit

---
+++ js/task-07.js
@@ -0,0 +1,11 @@
+const inputEl = document.getElementById('font-size-control');
+
+const inputRange = function () {
+    console.log(inputEl.type.this.value);
+};
+
+
+
+// inputEl.ge= function () {
+//     console.log(this.value);
+// };
