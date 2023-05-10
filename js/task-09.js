269c35efdbb0155b780c13ac92c47eb3e0117a91
ChesterWindsmoke <vladislavmarchenko999@gmail.com>
2023-05-10 18:43:19 +0300

    commit

--- js/task-09.js
+++ js/task-09.js
@@ -9,6 +9,7 @@ function changeColorFunction() {
   spanColor.textContent = getRandomHexColor();
 };
 
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
