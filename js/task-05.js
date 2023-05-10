4521aacfc353d16cb578447e6469e3863706d51e
ChesterWindsmoke <vladislavmarchenko999@gmail.com>
2023-05-10 18:14:05 +0300

    commit

--- js/task-05.js
+++ js/task-05.js
@@ -3,6 +3,7 @@
 
 const inputRef = document.querySelector('#name-input');
 const spanRef = document.querySelector('#name-output');
+const defValue = 'Anonymous';
 
 // Додаємо слухача на input, застосовуємо подію input , та передаємо функцію як колбек
 
@@ -10,5 +11,12 @@ inputRef.addEventListener('input', onInputChange);
 
 // Функція 
 function onInputChange(event) {
+    if (inputRef.value === "") {
+        spanRef.textContent = defValue;
+    } 
+    else {
     spanRef.textContent = event.currentTarget.value;
-}
+    }
+    }
+   
+
--- js/task-09.js
+++ js/task-09.js
@@ -1,13 +1,16 @@
 const changeColor = document.querySelector('.change-color');
+const spanColor = document.querySelector('.color')
 const body = document.querySelector('body');
 
-changeColor.addEventListener('click', getRandomHexColor);
+changeColor.addEventListener('click', changeColorFunction);
+
+function changeColorFunction() {
+  body.style.backgroundColor = getRandomHexColor();   
+  spanColor.textContent = getRandomHexColor();
+};
 
 function getRandomHexColor() {
-  body.style.backgroundColor= `#${Math.floor(Math.random() * 16777215)
+  return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
-}
-
-
-
+};
